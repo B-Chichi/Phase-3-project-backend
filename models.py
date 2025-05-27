8498e61c23acb7a9922636e9e95589c3e753bbcd
Brian <brian2000bett@gmail.com>
2025-05-27 15:02:56 +0300

    tables created

---
+++ models.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column,Integer,Text,ForeignKey
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, nullable=False)
+    price = Column(Integer, nullable=False)
+    stock = Column(Integer, default=0)
+    supplier_id = Column(Integer, ForeignKey("suppliers.id"))
+
+class Supplier(Base):
+    __tablename__ = "suppliers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, nullable=False)
+    contact_info = Column(Text)
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer, nullable=False)
+    customer_name = Column(Text, nullable=False)
+
+
+
+
+
+
+
