06bcf418aa8542e20a66a8aad71798c6f5ee1b82
Brian <brian2000bett@gmail.com>
2025-05-27 21:05:48 +0300

    schemas.py added

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI,Depends
+from fastapi.middleware.cors import CORSMiddleware
 from models import get_db,Product
+from schemas import ProductSchema
+from sqlalchemy.orm import Session
 
 app=FastAPI()
 
+app.add_middleware(CORSMiddleware,allow_origins=["*"])
+
 @app.get("/products")
-def get_products(session=Depends(get_db)):
+def get_products(session:Session=Depends(get_db)):
     products=session.query(Product).all()
     print("Product created succesfully")
     return products
 
 @app.post("/products")
-def add_product():
+def add_product(product=ProductSchema, session: Session = Depends(get_db)):
     return {"message":"Product created successfully"}
 
 @app.get("/products/{id}")
